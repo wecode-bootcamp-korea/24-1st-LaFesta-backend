92e055d806b237f2d7293bf52d57c3009e8dd8b1
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-09-01 20:09:48 +0900

    Add: Create users views/signup

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from users.views import SignupView
+
+urlpatterns = [
+    path('/signup', SignupView.as_view())
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json, re, bcrypt
 
-# Create your views here.
+from django.http     import JsonResponse
+from django.views    import View
+
+from users.models import User
+
+class SignupView(View):
+    def post(self, request):
+        data             = json.loads(request.body)
+        email_validation = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+        password_validation = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+        password = data['password']
+            
+        if User.objects.filter(email=data['email']).exists():
+            return JsonResponse({'MESSAGE':"ALREADY EXISTED EMAIL"}, status=400)
+
+        if not email_validation.match(data['email']):
+            return JsonResponse({"MESSAGE":"EMAIL_ERROR"}, status=400)
+
+        if not password_validation.match(data['password']):
+            return JsonResponse({"MESSAGE":"PASSWORD_ERROR"}, status=400)
+
+        hashed_password  = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        decoded_password = hashed_password.decode('utf-8')
+
+        User.objects.create(
+            name         = data['name'],
+            gender       = data['gender'],
+            email        = data['email'],
+            phone_number = data['phone_number'],
+            password     = decoded_password,
+            birthday     = data['birthday']
+            )
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
