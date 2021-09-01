b7e67556cc38b0e96ec11aa8bf8c3b0cb2ebefa3
YeJun <hayejun1013@naver.com>
2021-09-01 21:17:20 +0900

    Add : Create Product detail view GET API

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .           import views
 
 urlpatterns = [
+    path("/<int:product_id>", views.ProductDetailView.as_view()),
     path("/list", views.ProductListView.as_view()),
-    path("/<int:product_id>", views.ProductDetailView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -3,10 +3,32 @@ import json
 from django.http import JsonResponse
 from django.views import View
 
-from .models import Product
-
-class ProductListView(View):
-    
-
+from .models import Product, Image
 
 class ProductDetailView(View):
+    def get(self, request, **kwargs):
+        result = []
+        product = Product.objects.get(id=kwargs["product_id"])
+        colors = product.colors.all()
+        sizes = product.sizes.all()
+        images = Image.objects.filter(product=product.id)
+
+        result.append(
+            {
+                "name": product.name,
+                "price": product.price,
+                "section": product.type.category.section.name,
+                "category": product.type.category.name,
+                "type": product.type.name,
+                "fit": product.fit.name,
+                "description_summary": product.description.summary,
+                "description_extra_information": product.description.extra_information,
+                "colors": list(colors.values()),
+                "sizes": list(sizes.values()),
+                "images": list(images.values()),
+            }
+        )
+        return JsonResponse({"results": result}, status=200)
+
+
+class ProductListView(View):
