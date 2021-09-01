d19245c0b4dd3937b88dca390aed04ce324219cd
YeJun <hayejun1013@naver.com>
2021-09-01 09:45:19 +0900

    Add: Create Model Classes in orders, products, users app and apply review feedback

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    memo = models.TextField()
+    status = models.ForeignKey("OrderStatus", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "orders"
+
+
+class OrderStatus(models.Model):
+    DEPOSIT = "D"
+    ON_DELIVERY = "OD"
+    DELIVERY_COMPLETE = "DC"
+    PURCHASE_COMPLETE = "PC"
+
+    ORDER_CHOICE = [
+        (DEPOSIT, "deposit"),
+        (ON_DELIVERY, "on_delivery"),
+        (DELIVERY_COMPLETE, "delivery_complete"),
+        (PURCHASE_COMPLETE, "purchase_complete")
+    ]
+    status = models.CharField(max_length=32, choices=ORDER_CHOICE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "order_status"
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    status = models.ForeignKey("OrderItemStatus", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "order_products"
+
+
+class OrderItemStatus(models.Model):
+    EXCHANGE = "E"
+    EXCHANGE_COMPLETE = "EC"
+    REFUND = "R"
+    REFUND_COMPLETE = "RC"
+
+    ORDER_PRODUCTS_CHOICE = [
+        (EXCHANGE, "exchange"),
+        (EXCHANGE_COMPLETE, "exchange_complete"),
+        (REFUND, "refund"),
+        (REFUND_COMPLETE, "refund_complete")
+    ]
+    status = models.CharField(max_length=32, choices=ORDER_PRODUCTS_CHOICE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "order_products"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    MALE = "M"
+    FEMALE = "F"
+
+    GENDER_CHOICE = [
+        (MALE, "male"),
+        (FEMALE, "female")
+    ]
+
+    name         = models.CharField(max_length=32)
+    gender       = models.CharField(max_length=8, choices=GENDER_CHOICE)
+    email        = models.CharField(max_length=64)
+    phone_number = models.CharField(max_length=32)
+    password     = models.CharField(max_length=128)
+    birthday     = models.DateField()
+
+    class Meta:
+        db_table = "users"
