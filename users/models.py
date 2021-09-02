1cf8dd4ff91ed65888b5b40008517f5107b693c1
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-09-02 18:19:52 +0900

    Add: signin_second

--- users/models.py
+++ users/models.py
@@ -2,16 +2,9 @@ from django.db import models
 
 
 class User(models.Model):
-    MALE = "M"
-    FEMALE = "F"
-
-    GENDER_CHOICE = [
-        (MALE, "male"),
-        (FEMALE, "female")
-    ]
 
     name         = models.CharField(max_length=32)
-    gender       = models.CharField(max_length=8, choices=GENDER_CHOICE)
+    gender       = models.CharField(max_length=8)
     email        = models.CharField(max_length=64)
     phone_number = models.CharField(max_length=32)
     password     = models.CharField(max_length=128)
