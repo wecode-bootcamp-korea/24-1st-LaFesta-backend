30aa0b4f142bb03ead08afc737494317b809e9ef
YeJun <hayejun1013@naver.com>
2021-09-02 15:52:39 +0900

    Fix Detail view get method based on review

--- products/views.py
+++ products/views.py
@@ -5,29 +5,29 @@ from django.views import View
 
 from .models import Product, Image
 
+
 class ProductDetailView(View):
     def get(self, request, **kwargs):
-        result = []
+        result = {}
         product = Product.objects.get(id=kwargs["product_id"])
         colors = product.colors.all()
         sizes = product.sizes.all()
-        images = Image.objects.filter(product=product.id)
+        images = product.image_set.all()
 
-        result.append(
-            {
-                "name": product.name,
-                "price": product.price,
-                "section": product.type.category.section.name,
-                "category": product.type.category.name,
-                "type": product.type.name,
-                "fit": product.fit.name,
-                "description_summary": product.description.summary,
-                "description_extra_information": product.description.extra_information,
-                "colors": list(colors.values()),
-                "sizes": list(sizes.values()),
-                "images": list(images.values()),
-            }
-        )
+        result = {
+            "name": product.name,
+            "price": product.price,
+            "section": product.type.category.section.name,
+            "category": product.type.category.name,
+            "type": product.type.name,
+            "fit": product.fit.name,
+            "description_summary": product.description.summary,
+            "description_extra_information": product.description.extra_information,
+            "colors": list(colors.values()),
+            "sizes": list(sizes.values()),
+            "images": list(images.values()),
+        }
+
         return JsonResponse({"results": result}, status=200)
 
 
