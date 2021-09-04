fab80f9bbe1b39d0270f1fc0707502f1eda004e5
YeJun <hayejun1013@naver.com>
2021-09-04 13:12:46 +0900

    Modify: Modify order model and add migration files

--- orders/models.py
+++ orders/models.py
@@ -3,9 +3,10 @@ from django.db import models
 from core.models import TimeStamp
 
 class Order(TimeStamp):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    address = models.CharField(max_length=64)
-    memo = models.TextField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, null=True)
+    receiver = models.CharField(max_length=32, null=True)
+    receiver_phone_number = models.CharField(max_length=32, null=True)
+    address = models.CharField(max_length=64, null=True)
     status = models.ForeignKey("OrderStatus", on_delete=models.CASCADE)
 
     class Meta:
@@ -13,18 +14,7 @@ class Order(TimeStamp):
 
 
 class OrderStatus(TimeStamp):
-    DEPOSIT = "D"
-    ON_DELIVERY = "OD"
-    DELIVERY_COMPLETE = "DC"
-    PURCHASE_COMPLETE = "PC"
-
-    ORDER_CHOICE = [
-        (DEPOSIT, "deposit"),
-        (ON_DELIVERY, "on_delivery"),
-        (DELIVERY_COMPLETE, "delivery_complete"),
-        (PURCHASE_COMPLETE, "purchase_complete"),
-    ]
-    status = models.CharField(max_length=32, choices=ORDER_CHOICE)
+    status = models.CharField(max_length=32)
 
     class Meta:
         db_table = "order_status"
@@ -41,18 +31,7 @@ class OrderItem(TimeStamp):
 
 
 class OrderItemStatus(TimeStamp):
-    EXCHANGE = "E"
-    EXCHANGE_COMPLETE = "EC"
-    REFUND = "R"
-    REFUND_COMPLETE = "RC"
-
-    ORDER_PRODUCTS_CHOICE = [
-        (EXCHANGE, "exchange"),
-        (EXCHANGE_COMPLETE, "exchange_complete"),
-        (REFUND, "refund"),
-        (REFUND_COMPLETE, "refund_complete"),
-    ]
-    status = models.CharField(max_length=32, choices=ORDER_PRODUCTS_CHOICE)
+    status = models.CharField(max_length=32)
 
     class Meta:
         db_table = "order_products_status"
