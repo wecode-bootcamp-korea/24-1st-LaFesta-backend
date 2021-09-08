73462329b25618652ad28bd71c3fc38430d5086f
YeJun <hayejun1013@naver.com>
2021-09-08 18:28:46 +0900

    Add : add delete cart product function

--- orders/views.py
+++ orders/views.py
@@ -59,6 +59,22 @@ class CartView(View):
 
         return JsonResponse({"results": results}, status=200)
 
+    @login_decorator
+    def delete(self, request):
+        try:
+            data = json.loads(request.body)
+
+            OrderItem.objects.filter(
+                order__status_id=OrderStatus.Status.ON_CART.value,
+                product_id=data["product_id"],
+            ).delete()
+
+            return JsonResponse(
+                {"message": f"PRODUCT {data['product_id']} DELETED"}, status=200
+            )
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
 
 class OrderView(View):
     @login_decorator
